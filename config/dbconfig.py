320cd5f773a2cf3eedec5f19b40d24ec1c9d6828
Ramon <ramon.rosado2@upr.edu>
2019-05-11 01:12:55 -0400

    Fixed db config (was badly indexed)

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -7,21 +7,16 @@
 #     'dbname' : 'photoMsgApp'
 # }
 
-# 'user': 'luis',
-# 'passwd': 'Anib2013',
-# 'host': 'localhost',
-# 'dbname': 'photoMsgApp'
+# pg_config = {
+#     'user': 'carlos',
+#     'passwd': 'Tozanarkand',
+#     'host': 'localhost',
+#     'dbname': 'PhotoMsgApp'
+#     }
 
 pg_config = {
-    'user': 'carlos',
-    'passwd': 'Tozanarkand',
+    'user': 'ramon2',
+    'passwd': '12345678',
     'host': 'localhost',
-    'dbname': 'PhotoMsgApp'
-    }
-
-# pg_config = {
-# 'user': 'ramon2',
-# 'passwd': '12345678',
-# 'host': 'localhost',
-# 'dbname': 'photomsgapp'
-# }
+    'dbname': 'photomsgapp'
+}
