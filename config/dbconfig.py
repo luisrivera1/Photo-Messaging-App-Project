c7d9a653d1f8240dfa3ba8601e457894b000d02d
Ramon <ramon.rosado2@upr.edu>
2019-05-15 19:56:48 -0400

    Fixed dbconfig, user handler now catches an new error when username is left blank and contact list successfully refreshes

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,11 +1,11 @@
 # Database configuration information
 
-pg_config = {
-    'user' : 'luis',
-    'passwd' : 'Anib2013',
-    'host' : 'localhost',
-    'dbname' : 'photoMsgApp'
-}
+# pg_config = {
+#     'user' : 'luis',
+#     'passwd' : 'Anib2013',
+#     'host' : 'localhost',
+#     'dbname' : 'photoMsgApp'
+# }
 
 # 'user': 'luis',
 # 'passwd': 'Anib2013',
@@ -19,9 +19,9 @@ pg_config = {
 #     'dbname': 'PhotoMsgApp'
 #     }
 
-# pg_config = {
-# # 'user': 'ramon',
-# # 'passwd': '12345678',
-# # 'host': 'localhost',
-# # 'dbname': 'photomsgspp'
-# }
+pg_config = {
+'user': 'ramon2',
+'passwd': '12345678',
+'host': 'localhost',
+'dbname': 'photomsgapp'
+}
